dca912ac1ceef5792fdb04cbcd0bfa8d51d6e480
Rahulk2007 <iamrhulkakkar@gmail.com>
2025-08-25 20:03:31 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a,b ;
+	printf("enter two nos. \n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	int sum=0;
+	sum=a+b;
+	printf("\n The sum = %d", sum);
+	return 0 ;
+}
